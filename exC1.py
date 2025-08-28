c3b18c29b4fa2f7277f0b8a6da8bfbc118c15b98
DuyCaChef <duylun72@gmail.com>
2025-08-28 13:05:34 +0700

    Nop bai tap cac vi du C1

---
+++ exC1.py
@@ -0,0 +1,32 @@
+print('Hello, World!')
+ 
+print('Albert Einstein đã từng nói: Một người không bao giờ mắc sai lầm không bao giờ thủw bất kỳ điều gì mới.')
+
+if(5>2):
+    print('Five is greater than two!')
+
+# Mở rộng câu lệnh
+total = 1 + \
+2 + \
+3
+print(total); print('Hello Python!')
+
+# Minh hoạ câu lệnh if...else
+if True:
+    print('True')
+else:
+    print('False')  
+    
+#math_libs.py
+import math_libs
+print(math_libs.add(2, 6))
+
+#module1.py
+import module1
+print(module1.question)
+print(module1.answer)
+
+#module2.py
+import module2
+print(module2.question)
+print(module2.answer)
